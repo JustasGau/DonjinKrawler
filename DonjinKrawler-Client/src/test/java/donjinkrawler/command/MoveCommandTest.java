ea0e1334564defc0893bf43970fc66b5f26992c9
Justas Gaurylius <justas.gaurylius@gmail.com>
2020-11-16 21:05:51 +0200

    command and decorator tests

---
+++ DonjinKrawler-Client/src/test/java/donjinkrawler/command/MoveCommandTest.java
@@ -0,0 +1,54 @@
+package donjinkrawler.command;
+
+import com.esotericsoftware.kryonet.Client;
+import donjinkrawler.Player;
+import krawlercommon.PlayerData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoveCommandTest {
+
+    @Test
+    public void testMoveCommandConstructor1() {
+        Player testPlayer = new Player(new PlayerData(), new Client());
+        MoveCommand mv = new MoveCommand(testPlayer, 30, 20);
+        assertEquals(30, mv.dx);
+        assertEquals(20, mv.dy);
+    }
+
+    @Test
+    public void testMoveCommandConstructor2() {
+        Player testPlayer = new Player(new PlayerData(), new Client());
+        testPlayer.setX(50);
+        testPlayer.setY(60);
+        MoveCommand mv = new MoveCommand(testPlayer, 30, 20);
+        assertEquals(50, mv.oldX);
+        assertEquals(60, mv.oldY);
+    }
+
+    @Test
+    public void testMoveCommandExecute() {
+        PlayerCommander commander = new PlayerCommander();
+        Player testPlayer = new Player(new PlayerData(), new Client());
+        testPlayer.setX(100);
+        testPlayer.setY(200);
+        MoveCommand mv = new MoveCommand(testPlayer, 30, 20);
+        commander.execute(mv);
+        assertEquals(130, testPlayer.getX());
+        assertEquals(220, testPlayer.getY());
+    }
+
+    @Test
+    public void testMoveCommandUndo() {
+        PlayerCommander commander = new PlayerCommander();
+        Player testPlayer = new Player(new PlayerData(), new Client());
+        testPlayer.setX(100);
+        testPlayer.setY(200);
+        MoveCommand mv = new MoveCommand(testPlayer, 30, 20);
+        commander.execute(mv);
+        commander.undo();
+        assertEquals(100, testPlayer.getX());
+        assertEquals(200, testPlayer.getY());
+    }
+}
