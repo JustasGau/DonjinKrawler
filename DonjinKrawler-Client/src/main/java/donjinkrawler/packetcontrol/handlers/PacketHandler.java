28567cb52a0b92b6b011a5eb685f147d7d127b41
arvapu <arvydas.apulskis123@gmail.com>
2020-12-02 22:45:04 +0200

    redurant comments removed

--- DonjinKrawler-Client/src/main/java/donjinkrawler/packetcontrol/handlers/PacketHandler.java
+++ DonjinKrawler-Client/src/main/java/donjinkrawler/packetcontrol/handlers/PacketHandler.java
@@ -6,27 +6,13 @@ public abstract class PacketHandler {
 
     private PacketHandler next;
 
-    /**
-     * Builds chains of middleware objects.
-     */
     public PacketHandler linkWith(PacketHandler next) {
         this.next = next;
         return next;
     }
 
-    /**
-     * Subclasses will implement this method with concrete checks.
-     *
-     * @param request
-     */
     public abstract boolean handle(Request request);
 
-    /**
-     * Runs check on the next object in chain or ends traversing if we're in
-     * last object in chain.
-     *
-     * @param request
-     */
     protected boolean next(Request request) {
         if (next == null) {
             return true;
